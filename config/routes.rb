b5f1dd46e054cef921a448bfa9e0e8cadfc68f4d
TIMM SNOW <two.tone.snow@gmail.com>
2021-03-09 17:44:39 -0500

    creates auth app and 2 users, enables authentication and creates a show file when session is authenticated it prints user info

---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+  # EXAMPLE HTML ROUTE
+  # get "/photos" => "photos#index"
+
+  # EXAMPLE JSON ROUTE WITH API NAMESPACE
+  namespace :api do
+    post "/users" => "users#create"
+
+    post "/sessions" => "sessions#create"
+    get "/sessions" => "sessions#show"
+  end
+end
